de3fdd7cb822eaa213e5b1f57bbb23d9a9dcee4e
AJZ <jinnawat202@gmail.com>
2018-01-18 17:04:46 +0700

    add detection for invalid username

--- src/UserManagerMain.java
+++ src/UserManagerMain.java
@@ -9,8 +9,28 @@ public class UserManagerMain {
 		for(int i = 0; i < 3 ; i++)
 		{
 			username = JOptionPane.showInputDialog("Please Enter new user's username");
+			while(username == null || username.matches("") || containsRestrictedCharacter(username))
+			{
+				username = JOptionPane.showInputDialog("Please Reenter new user's username");
+			}
 			X.addNewUser(username);
 		}
 		X.showAllUser();
+	}
+	
+	
+	public static boolean containsRestrictedCharacter(String ToBeChecked)
+	{
+		char[] resChar = {'<' , '>', ':','/','\\','|','?','*',' '};
+		char[] checking = ToBeChecked.toCharArray();
+		for(int i = 0; i < ToBeChecked.length() ; i++) //for all char in string
+		{
+			for(int j = 0 ; j < resChar.length ; j++)
+			{
+				if(checking[i]==resChar[j])
+					return true;
+			}
+		}
+		return false;
 	}
 }
